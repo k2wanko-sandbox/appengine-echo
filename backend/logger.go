f56366dae0cbfa490ecc8ba8a4f2ffd07f98ca0e
Kazuhiro Kubota <k2.wanko@gmail.com>
2016-11-17 21:47:43 +0900

    Update echo

--- backend/logger.go
+++ backend/logger.go
@@ -17,13 +17,13 @@ func Logger() echo.MiddlewareFunc {
 			}
 			end := time.Now()
 			latency := end.Sub(start)
-			ip := req.RemoteAddress()
-			method := req.Method()
-			p := req.URL().Path()
+			ip := req.RemoteAddr
+			method := req.Method
+			p := req.URL.Path
 			if p == "" {
 				p = "/"
 			}
-			s := res.Status()
+			s := res.Status
 			o := fmt.Sprintf("%v | %3d | %s | %s | %-7s | %s",
 				end.Format("2006/01/02 - 15:04:05"),
 				s,
--- backend/server.go
+++ backend/server.go
@@ -3,42 +3,29 @@ package backend
 import (
 	"net/http"
 
-	ae "github.com/k2wanko/echo-appengine"
+	"golang.org/x/net/context"
+
 	"github.com/labstack/echo"
-	"github.com/labstack/echo/engine/standard"
 	"google.golang.org/appengine"
 )
-
-func New() *echo.Echo {
-	e := echo.New()
-	e.Use(ae.AppContext())
-	e.Use(ae.AppLogger())
-	e.Use(Logger())
-	e.GET("/", handleIndex)
-	return e
-}
 
 func init() {
 	if err := loadConfig(); err != nil {
 		panic(err)
 	}
 
-	s := standard.New("")
-	s.SetHandler(New())
-	http.Handle("/", s)
+	e := echo.New()
+	e.Use(Logger())
+	e.GET("/", handleIndex)
+
+	http.Handle("/", e)
 }
 
 func handleIndex(c echo.Context) error {
-	return c.String(http.StatusOK, "index: "+appengine.AppID(c))
+	ctx := newContext(c)
+	return c.String(http.StatusOK, "index: "+appengine.AppID(ctx))
 }
 
-func AppContext() echo.MiddlewareFunc {
-	return func(next echo.HandlerFunc) echo.HandlerFunc {
-		return func(c echo.Context) error {
-			if r, ok := c.Request().(*standard.Request); ok {
-				c.SetContext(appengine.WithContext(c.Context(), r.Request))
-			}
-			return next(c)
-		}
-	}
+func newContext(c echo.Context) context.Context {
+	return appengine.NewContext(c.Request())
 }
